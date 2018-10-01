1ffed3f726210743af48bb30ea299e9649880962
merajabi <43745885+merajabi@users.noreply.github.com>
2018-10-01 17:08:23 +0330

    search

--- test/test1.cpp
+++ test/test1.cpp
@@ -6,21 +6,33 @@ using namespace PlusORM;
 
 int main(int argc, char **argv) {
 
-	ORM<Person>* model;
+	{
+		ORM<Person>* model = ORM<Person>::GetInstance(); // Get an instance of ORM 
+		Person::Initialize(model->MaxPrimaryKey());      // Initialize Person (sync with db)
 
-	model=ORM<Person>::GetInstance();
-	Person::Initialize(model->MaxPrimaryKey());
+		Person john ("John", "Doe", 33);                 // Create new Person object
 
-	Person john ("John", "Doe", 33);
+		model->Insert(john);                             // Insert object in DB
 
-	model->Insert(john);
+		std::cout << model->Count() << std::endl;        // Number of Person table row in DB
 
-	std::cout << model->Count() << std::endl;
+		john.SetAge(34);                                // change the Person object
+		model->Update(john);                             // Update the DB
+	}
+	{
+		ORM<Person>* model = ORM<Person>::GetInstance(); // Get an instance of ORM 
+		Person::Initialize(model->MaxPrimaryKey());      // Initialize Person (sync with db)
 
-	model->Drop();
+		model->Search("*","Age > 30");					 // Search persons with Age > 30
+		const std::list<Person*> list=model->GetResultList();
+		for(std::list<Person*>::const_iterator it=list.begin(); it != list.end(); ++it){
+			Person* ptr = *it;
+			std::cout << ptr->GetId() << "\t" << ptr->GetFirst() << "\t" << ptr->GetLast() << "\t" << ptr->GetAge () << std::endl;
+		}
+		//model->Drop();                                   // Drom Person table in DB, if you wish
+	}
 
-	ORM<Person>::RemoveInstance();
+	ORM<Person>::RemoveInstance();                   // Remove ORM instance, just at the end of program.
 	std::cout << "Finish" << std::endl;
 	return 0;
 }
-
