7554b6882e1ca95b0b91405db614c2c30302ae7f
merajabi <43745885+merajabi@users.noreply.github.com>
2018-10-01 17:07:01 +0330

    build

--- build.sh
+++ build.sh
@@ -10,21 +10,16 @@ if [ "$TARGET" = "X86" ]
 then
 	make dynamiclib ARCHITECTURE="$ROOTFOLDER/x86.mk" MANAGER="$ROOTFOLDER/makefile.mk" BUILD="sqlite3.24"; > out.txt 2>&1
 	cat out.txt | grep --color -E "error:"
-fi
-
-if [ "$TARGET" = "clean" ]
+elif [ "$TARGET" = "clean" ]
 then
 	make cleanall MANAGER="$ROOTFOLDER/makefile.mk" BUILD="sqlite3.24" ;
 	rm test/*.so test/*.a test/*.o test/*.out
-fi
-
-
-if [ "$TARGET" = "test" ]
+elif [ "$TARGET" = "test" ]
 then
 	cd ./test
 	./build-tests.sh
 	cd ..
+else
+	make dynamiclib ARCHITECTURE="$ROOTFOLDER/$TARGET.mk" MANAGER="$ROOTFOLDER/makefile.mk" BUILD="sqlite3.24"; > out.txt 2>&1
+	cat out.txt | grep --color -E "error:"
 fi
-
-
-
